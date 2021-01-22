04886b33057f3cfaa9a9d9d9ad091c8ffae9165c
bipin247 <bipin247@gmail.com>
2021-01-22 10:04:18 +0000

    send receive mq message with internal server setup

---
+++ src/main/java/guru/springframework/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static long serialVersionUID = -8440989717406210437L;
+
+    private UUID id;
+    private String message;
+}
